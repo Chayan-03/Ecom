22313f187a040b20a3e47d9ced66d912f11f130e
Chayan-03 <chayan.06014802722@cse.mait.ac.in>
2025-07-18 14:01:31 +0530

    error resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { Navbar } from './components/Navbar.jsx'
 import {Routes, Route} from 'react-router-dom'
 import {BrowserRouter} from "react-router-dom";
 import { Homepage } from './pages/Homepage'
 import {GetAllProducts} from './api/getAllProducts'
 import {CartProvider} from "./context/cart-context";
 import {Cartpage} from "./pages/Cartpage";
-import {ProfilePage} from "./pages/ProfilePage.jsx";
-import {Whishlist} from "./pages/Whishlist.jsx";
+import {ProfilePage} from "./pages/ProfilePage";
+import {Whishlist} from "./pages/Whishlist";
 
 
 
